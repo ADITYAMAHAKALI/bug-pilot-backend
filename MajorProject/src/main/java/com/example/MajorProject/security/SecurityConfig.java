7cdd72e205b9a599ffbf2da754700e4e7ef062e3
HARENDER SINGH <89127398+harender-singh-chauhan@users.noreply.github.com>
2023-03-27 10:36:11 +0530

    some quick fixes are done

--- MajorProject/src/main/java/com/example/MajorProject/security/SecurityConfig.java
+++ MajorProject/src/main/java/com/example/MajorProject/security/SecurityConfig.java
@@ -1,43 +1,43 @@
-// package com.example.MajorProject.security;
+package com.example.MajorProject.security;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-// import static org.springframework.security.config.Customizer.withDefaults;
-
-// @Configuration
-// public class SecurityConfig {
-//     // Adding roles
-//     @Bean
-//     public InMemoryUserDetailsManager userDetailsManager(){
-//         UserDetails hartej=User.builder().username("hartej").password("{noop}123").roles("Employee").build();
-//         UserDetails aditya=User.builder().username("aditya").password("{noop}123").roles("Manager","Employee").build();
-//         UserDetails disha=User.builder().username("disha").password("{noop}123").roles("Employee","Manager","Admin").build();
-//         //UserDetails x=User
-//         return new InMemoryUserDetailsManager(hartej,aditya,disha);
-//     }
-//     // Restricting URL based upon roles
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
-//     {
-//         http.authorizeHttpRequests(configurer ->
-//             configurer
-//             .antMatchers(HttpMethod.GET,"./api/bugs").hasRole("Employee")
-//             .antMatchers(HttpMethod.GET,"./api/bugs/**").hasRole("Employee")
-//             .antMatchers(HttpMethod.POST,"./api/employees").hasRole("Manager")
-//             .antMatchers(HttpMethod.PUT,"./api/employees").hasRole("Manager")
-//             .antMatchers(HttpMethod.DELETE,"./api/employees/**").hasRole("Admin")
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+     // Adding roles
+     @Bean
+     public InMemoryUserDetailsManager userDetailsManager(){
+         UserDetails hartej=User.builder().username("hartej").password("{noop}123").roles("Employee").build();
+         UserDetails aditya=User.builder().username("aditya").password("{noop}123").roles("Manager","Employee").build();
+         UserDetails disha=User.builder().username("disha").password("{noop}123").roles("Employee","Manager","Admin").build();
         
-//         );
-//         http.httpBasic(withDefaults());
-//         //http.csrf(null);
-//         http.csrf(withDefaults());
-//         return http.build();
-//     }
-// }
+         return new InMemoryUserDetailsManager(hartej,aditya,disha);
+     }
+     
+     // Restricting URL based upon roles
+     @Bean
+     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+     {
+         http.authorizeHttpRequests(configurer ->
+             configurer
+             .antMatchers(HttpMethod.GET,"./api/bugs").hasRole("Employee")
+             .antMatchers(HttpMethod.GET,"./api/bugs/**").hasRole("Employee")
+             .antMatchers(HttpMethod.POST,"./api/bugs/").hasRole("Manager")
+             .antMatchers(HttpMethod.PUT,"./api/bugs/").hasRole("Manager")
+             .antMatchers(HttpMethod.DELETE,"./api/bugs/**").hasRole("Admin")
+        
+         );
+         http.httpBasic();
+         http.csrf().disable();
+         return http.build();
+     }
+ }
